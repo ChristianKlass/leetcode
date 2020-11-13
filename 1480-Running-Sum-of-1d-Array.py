9817ea46bbb6fff0d897f0cef42f77755d3cba9f
Mark Klass <mark_christian@zoho.com>
2020-11-13 13:26:26 +0800

    added number 1480 - Running Sum of 1d Array

---
+++ 1480-Running-Sum-of-1d-Array.py
@@ -0,0 +1,14 @@
+def runningSum(nums):
+  sum_list = []
+
+  return sum_list
+
+
+def main():
+    num_list = [1, 2, 3, 4, 5]
+
+    print(runningSum(num_list))
+
+
+if __name__ == "__main__":
+    main()
