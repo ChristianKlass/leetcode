b61478a8b53163f06b47f49bd42d77c531957067
Mark Klass <mark_christian@zoho.com>
2020-11-13 13:34:42 +0800

    completed, but not very efficient. TODO: look into a way to make it more efficient

--- 1480-Running-Sum-of-1d-Array.py
+++ 1480-Running-Sum-of-1d-Array.py
@@ -1,11 +1,15 @@
 def runningSum(nums):
   sum_list = []
+  sum_list.append(nums[0])
+
+  for i in range(1, len(nums)):
+    sum_list.append(sum_list[i - 1] + nums[i])
 
   return sum_list
 
 
 def main():
-    num_list = [1, 2, 3, 4, 5]
+    num_list = [1, 2, 3, 4]
 
     print(runningSum(num_list))
 
